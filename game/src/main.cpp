2228d50e55e720b5a1f7f00b12918d800146b4b0
Yapomip <zenovaromir@gmail.com>
2024-10-24 23:43:23 +0300

    SDL2 connect

---
+++ game/src/main.cpp
@@ -0,0 +1,12 @@
+
+#include "Windows.h"
+
+#include "SDL2\include\SDL.h"
+
+#pragma comment(lib, "SDL2.lib")
+
+
+INT WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nShowCmd )
+{
+    return 0;
+}
